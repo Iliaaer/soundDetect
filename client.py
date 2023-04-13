a68ebc0198a7dab94b4f38e39ed273afd3dec36e
iliaaer <zarubinilia@yandex.ru>
2023-04-14 00:17:27 +0300

    first commit

--- client.py
+++ client.py
@@ -1,17 +1,25 @@
 from pyannote.audio import Pipeline
 import whisper
+
+def millisec(timeStr):
+    spl = timeStr.split(":")
+    s = (int)((int(spl[0]) * 60 * 60 + int(spl[1]) * 60 + float(spl[2]) )* 1000)
+    return s
+
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
                                     use_auth_token="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")
 
-model = whisper.load_model("medium")
-# model = whisper.load_model("large")
+# model = whisper.load_model("medium")
+model = whisper.load_model("large-v2")
+print(model.is_multilingual)
+
 
 FILE_NAME = "5"
 
 diarization = pipeline("wav/" + FILE_NAME + ".wav")
 
 for turn, _, speaker in diarization.itertracks(yield_label=True):
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+    print(f"start={turn.start*1000}s stop={turn.end*1000}s speaker_{speaker}")
     
 # print(diarization, type(diarization))
 
