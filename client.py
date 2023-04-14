9a9de34f53831bbba9786ddd900c6c02c6e174a8
iliaaer <zarubinilia@yandex.ru>
2023-04-14 15:07:00 +0300

    1

--- client.py
+++ client.py
@@ -14,17 +14,24 @@ pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
 # model = whisper.load_model("medium")
 model = whisper.load_model(MODEL_NAME)
 audio = whisper.load_audio("wav/" + FILE_NAME + ".wav")
-options = whisper.DecodingOptions(language=LANGUAGE_WHISPER, fp16=False)
 
-# diarization = pipeline("wav/" + FILE_NAME + ".wav", min_speakers=MIN_SPEAKERS, max_speakers=MAX_SPEAKERS)
+diarization = pipeline("wav/" + FILE_NAME + ".wav", min_speakers=MIN_SPEAKERS, max_speakers=MAX_SPEAKERS)
 
-# for turn, _, speaker in diarization.itertracks(yield_label=True):
-#     print(f"start={turn.start*1000}s stop={turn.end*1000}s speaker_{speaker}")
+for turn, _, speaker in diarization.itertracks(yield_label=True):
+    print(f"start={turn.start}s stop={turn.end}s speaker_{speaker}")
 
-result = model.transcribe(audio, **options.__dict__, verbose=False)
-jsonResult = json.dumps(result)
-with open('detect/audio_text_' + FILE_NAME + '.txt', 'w') as outfile:
-    json.dump(jsonResult, outfile)
+result = model.transcribe(audio, language=LANGUAGE_WHISPER, fp16=False, verbose=True)
+# jsonResult = json.dumps(result, indent=4, ensure_ascii=False)
+# with open('detect/audio_text_' + FILE_NAME + '.json', 'w') as outfile:
+#     json.dump(jsonResult, outfile, indent=2)
 
-# with open("detect/audio_" + FILE_NAME + ".rttm", "w") as rttm:
-#     diarization.write_rttm(rttm)
+# print(result["text"])
+
+with open("detect/audio_" + FILE_NAME + ".rttm", "w") as rttm:
+    diarization.write_rttm(rttm)
+
+'''
+
+
+
+'''
